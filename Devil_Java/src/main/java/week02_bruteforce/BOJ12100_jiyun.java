65056c3cd77b571d9c57f2ae9dccb050e112000c
jiyun921 <ryn0921@naver.com>
2025-09-15 16:27:50 +0900

    [fix]: 불필요한 코드 정리

--- Devil_Java/src/main/java/week02_bruteforce/BOJ12100_jiyun.java
+++ Devil_Java/src/main/java/week02_bruteforce/BOJ12100_jiyun.java
@@ -8,7 +8,6 @@ public class BOJ12100_jiyun {
     static String[] direcction = {"up", "down", "right", "left"};
     static int n;
     static int max;
-    static boolean found = false;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,11 +25,9 @@ public class BOJ12100_jiyun {
 
         dfs(graph, 0);
         System.out.println(max);
-
     }
 
     static void dfs(int[][] graph, int depth) {
-
         if (depth == 5) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
@@ -46,23 +43,25 @@ public class BOJ12100_jiyun {
                 System.arraycopy(graph[j], 0, copy_graph[j], 0, n);
             }
 
-//            switch (d) {
-//                case "up" -> move_up(copy_graph);
-//                case "down" -> move_down(copy_graph);
-//                case "left" -> move_left(copy_graph);
-//                default -> move_right(copy_graph);
-//            }
-
+            // java 14+
             switch (d) {
-                case "up":
-                    move_up(copy_graph); break;
-                case "down":
-                    move_down(copy_graph); break;
-                case "left":
-                    move_left(copy_graph); break;
-                case "right":
-                    move_right(copy_graph); break;
+                case "up" -> move_up(copy_graph);
+                case "down" -> move_down(copy_graph);
+                case "left" -> move_left(copy_graph);
+                default -> move_right(copy_graph);
             }
+
+            // java 11
+//            switch (d) {
+//                case "up":
+//                    move_up(copy_graph); break;
+//                case "down":
+//                    move_down(copy_graph); break;
+//                case "left":
+//                    move_left(copy_graph); break;
+//                case "right":
+//                    move_right(copy_graph); break;
+//            }
 
             dfs(copy_graph, depth + 1);
         }
