d93c0c7bcfa354c23d922183b46a3086b90a60db
jiyun921 <ryn0921@naver.com>
2025-09-16 15:34:00 +0900

    [chore] 2주차 파일 정리

---
+++ Devil_Python/week02_bruteforce/BOJ12100_dwoong.py
@@ -0,0 +1,107 @@
+import sys
+
+board = []
+
+N = int(sys.stdin.readline().strip()) #\n 없애기 .strip()
+
+#board 만들어
+for row in range(N):
+    line = sys.stdin.readline().split()
+    values = []
+    for x in line:
+        values.append(int(x))
+    board.append(values)
+
+
+#왼쪽 기준
+def sum_line(line):
+
+    new_line = []
+    for k in line:
+        if k != 0:
+            new_line.append(k)
+
+    result = []
+
+    combined = False #합쳐진거 이미 건너뛰어야돼
+
+    for i in range(len(new_line)):
+        if combined:
+            combined = False
+            continue
+            #합쳐졌으면 건너뛰고 다음으로 걍 진행
+        if i + 1 < len(new_line) and new_line[i] == new_line[i+1]:
+            result.append(new_line[i] * 2)
+            combined = True
+
+        else:
+            result.append(new_line[i])
+
+    while len(result) < N:
+        result.append(0)
+
+    return result
+
+def move(board, direction):
+    new_board = []
+
+    for k in range(N):
+        row = []
+        for j in range(N):
+            row.append(0)
+        new_board.append(row)
+
+    #왼쪽
+    if direction == 0:
+        for i in range(N):
+            new_board[i] = sum_line(board[i])
+
+    #오른쪽
+    elif direction == 1:
+        for i in range(N):
+            new_board[i] = sum_line(board[i][::-1])[::-1] #보드 뒤집어서 하기 왜냐면 왼쪽 기준이니까
+
+    #위쪽
+    #음므믐ㅁㅁ 왼쪽 기준이니까 세로 계산해서 하기
+    elif direction == 2:
+        for i in range(N):
+            col = []
+            for j in range(N):
+                col.append(board[j][i])
+
+            new_col = sum_line(col)
+
+            for j in range(N):
+                new_board[j][i] = new_col[j]
+
+    #아래쪽
+    #위쪽 뒤짐으면 되지 않나
+    elif direction == 3:
+        for i in range(N):
+            col = []
+            for j in range(N-1, -1, -1):
+                col.append(board[j][i])
+
+            new_col = sum_line(col)[::-1]
+
+            for j in range(N):
+                new_board[j][i] = new_col[j]
+
+    return new_board
+
+
+#최대 5번까지 해보기 for문 5번?
+output = 0
+for d1 in range(4):
+    for d2 in range(4):
+        for d3 in range(4):
+            for d4 in range(4):
+                for d5 in range(4):
+                    directions = [d1, d2, d3, d4, d5]
+                    copy_board = board
+                    for d in directions:
+                        copy_board = move(copy_board, d)
+                    for i in range(N):
+                        output = max(output, max(copy_board[i]))
+
+print(output)
---
+++ Devil_Python/week02_bruteforce/BOJ16463_yujeong.py
@@ -0,0 +1,42 @@
+# 13일의 금요일 수 세기
+# 2019~사용자 입력 년도 까지
+
+def calendar(year, month, day):
+    # 1월, 2월은 전년도 13월, 14월로 계산
+    # 윤년 계산 단순화 위해.. 젤러씨가 그럼
+    if month == 1:
+        month = 13
+        year -= 1
+    elif month == 2:
+        month = 14
+        year -= 1
+
+    # Zeller's Congruence
+    # 0=토, 1=일, 2=월, 3=화, 4=수, 5=목, 6=금
+    return (day + (13 * (month + 1)) // 5 + year + (year // 4) - (year // 100) + (year // 400)) % 7
+
+def friday13(end):
+    start = 2019 # 시작년도
+    total = end - start + 1 # 총 년도 수
+
+    cycle_count = 0 # 400년 동안의 13일의 금요일 수
+
+    # 400년 주기 동안의 13일의 금요일 수 세기
+    for year in range(start, start + 400):
+        for month in range(1, 13):
+            if calendar(year, month, 13) == 6: # 현재 연도/월/13일이 금요일(6)이면
+                cycle_count += 1
+
+    cycle = total // 400 # 400년 주기 수
+    remainder_year = total % 400 # 나머지 년도 수
+    count = cycle_count * cycle # 전체 400년 주기에서의 13일의 금요일 수
+
+    # 나머지 년도에서의 13일의 금요일 수
+    for year in range(start, start + remainder_year): # 지정된연도 범위 순회
+        for month in range(1, 13): # 월 범위 순회
+            if calendar(year, month, 13) == 6: # 현재 연도/월/13일이 금요일(6)이면
+                count += 1        
+    return count
+
+n = int(input())
+print(friday13(n))
