5b18b0d1570f7d7c8faa6b175b6784fd489672d1
jiyun921 <ryn0921@naver.com>
2025-09-19 16:45:08 +0900

    [feat]: week03_BOJ1744 - 수 묶기 구현

---
+++ Devil_Java/src/main/java/week03_greedy/jiyun/BOJ1744.java
@@ -0,0 +1,63 @@
+package week03_greedy.jiyun;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1744 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] num = new int[n];
+
+        for (int i=0; i<n; i++) {
+            num[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(num);
+
+        int result = 0;
+
+        int posIdx = 0;
+        for (int i=0; i<n; i++) {
+            if (i < n-1 && num[i]<0 && num[i+1]<0) {
+                result += num[i]*num[++i];
+            }
+            else if (i < n-1 && num[i]<0 && num[i+1]==0) {
+                posIdx = i+2;
+                break;
+            }
+            else if (num[i]<0) {
+                result += num[i];
+                posIdx = i+1;
+                break;
+            }
+            else {
+                posIdx = i;
+                break;
+            }
+        }
+
+        boolean isFirstIdx=true;
+        for (int i=posIdx; i<n; i++) {
+            if (num[i]==0 || num[i]==1) {
+                result += num[i];
+            }
+            else if (isFirstIdx) {
+                if ((n-i)%2!=0) {
+                    result += num[i];
+                }
+                else {
+                    result += num[i] * num[++i];
+                }
+                isFirstIdx = false;
+            }
+            else {
+                result += num[i] * num[++i];
+            }
+        }
+
+        System.out.println(result);
+    }
+}
