8059a9cada3ac39d281c885715ba873b901d2b58
Sanghun Park <ptw06030@gmail,com>
2025-09-22 16:44:17 +0900

    BOJ_1744 상훈이꺼

--- Devil_C/week03_greedy/sanghun/BOJ1744.c
+++ Devil_C/week03_greedy/sanghun/BOJ1744.c
@@ -15,28 +15,28 @@ int main() {
     int *arr = (int*)malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
 
-    int positives[100], negatives[100];
+    int pos[100], neg[100];
     int pCnt = 0, nCnt = 0, ones = 0, zeros = 0;
 
     for (int i = 0; i < n; i++) {
-        if (arr[i] > 1) positives[pCnt++] = arr[i];
+        if (arr[i] > 1) pos[pCnt++] = arr[i];
         else if (arr[i] == 1) ones++;
         else if (arr[i] == 0) zeros++;
-        else negatives[nCnt++] = arr[i];
+        else neg[nCnt++] = arr[i];
     }
 
-    qsort(positives, pCnt, sizeof(int), desc);
-    qsort(negatives, nCnt, sizeof(int), asc); 
+    qsort(pos, pCnt, sizeof(int), desc);
+    qsort(neg, nCnt, sizeof(int), asc); 
 
     long long sum = 0;
 
     for (int i = 0; i + 1 < pCnt; i += 2)
-        sum += positives[i] * positives[i + 1];
-    if (pCnt % 2 == 1) sum += positives[pCnt - 1];
+        sum += pos[i] * pos[i + 1];
+    if (pCnt % 2 == 1) sum += pos[pCnt - 1];
 
     for (int i = 0; i + 1 < nCnt; i += 2)
-        sum += negatives[i] * negatives[i + 1];
-    if (nCnt % 2 == 1 && zeros == 0) sum += negatives[nCnt - 1];
+        sum += neg[i] * neg[i + 1];
+    if (nCnt % 2 == 1 && zeros == 0) sum += neg[nCnt - 1];
 
     sum += ones;
 
