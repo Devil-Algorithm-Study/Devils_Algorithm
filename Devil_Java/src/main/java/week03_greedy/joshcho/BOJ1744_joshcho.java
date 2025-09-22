4f190154b874a603e5d23d9b6b9875b15cf35bea
조성호 <105763253+Joshcho426@users.noreply.github.com>
2025-09-22 17:38:13 +0900

    Update BOJ1744_joshcho.java

--- Devil_Java/src/main/java/week03_greedy/joshcho/BOJ1744_joshcho.java
+++ Devil_Java/src/main/java/week03_greedy/joshcho/BOJ1744_joshcho.java
@@ -28,18 +28,10 @@ public class BOJ1744_joshcho {
         Collections.sort(negatives);
 
         // 2개씩 묶어서 곱할 때 전체 입력받은 개수가 홀/짝인지 분기처리
-        if(positives.size() % 2 == 0) {
-            for(int i = 0; i < positives.size() - 1; i += 2) {
-                sum += positives.get(i) * positives.get(i+1);
-            }
-        }
-        else
-        {
-            for(int i = 0; i < positives.size() -1; i += 2) {
-                sum += positives.get(i) * positives.get(i+1);
-            }
-             sum += positives.get(positives.size()-1);
-        }
+        for(int i = 0; i < positives.size() - 1; i += 2) {
+            sum += positives.get(i) * positives.get(i+1);
+        } 
+        if(positives.get(i) % 2 == 1) sum += positives.get(positives.size()-1);
 
         if(negatives.size() % 2 == 0) {
             for(int i = 0; i < negatives.size() - 1; i += 2) {
